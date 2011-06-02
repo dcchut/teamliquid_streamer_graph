d88c7e27cd222e67f42eefb20837bdc1b3cf816b
robbo <dcchut@gmail.com>
2011-06-02 18:17:04 +1000

    updated text to reflect new nature of graphing utility

--- graph/index.php
+++ graph/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>teamliquid streamer graph</title>
+        <title>teamliquid featured streamers graph</title>
         <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="./flot/excanvas.min.js"></script><![endif]-->
         <script language="javascript" type="text/javascript" src="./flot/jquery.js"></script>
         <script language="javascript" type="text/javascript" src="./flot/jquery.flot.js"></script>
@@ -9,9 +9,9 @@
     </head>
     <body>
 	<div id="main">
-    <b>teamliquid streamer graph</b> - presented by dcchut<br /><br />
+    <b>teamliquid featured streamers graph</b> - presented by <a href="http://dcc.nitrated.net">dcchut</a><br /><br />
     <div id="graph" style="width:1500px;height:700px;"></div><br />
-    all times expressed in AEST - graph updates every 35 seconds -  <a href="http://dcc.nitrated.net/">my bio</a></div>
+    all times expressed in AEST - graph updates every 35 seconds</div>
     <script type="text/javascript">
 		setTimeout("location.reload(true);", 3600 * 1000);
 	</script>
